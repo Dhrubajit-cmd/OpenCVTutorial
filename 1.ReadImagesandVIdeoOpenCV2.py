b6e3041446f085e0f6a776c52a8ea0c2f7b09d51
Dhrubajit Chakravarty <dhrubajitchakravorty@gmail.com>
2024-09-02 00:21:09 +0530

    A commit

--- 1.ReadImagesandVIdeoOpenCV2.py
+++ 1.ReadImagesandVIdeoOpenCV2.py
@@ -2,6 +2,7 @@
 import cv2 as cv
 import os
 
+# Reading Images in OpenCV
 img_path = "/home/dc/Pictures/Wallpapers/evening.png"
 if not os.path.isfile(img_path) :
     print(f"Error, file {img_path} does not exist or not found. \n")
@@ -10,8 +11,10 @@ else :
     if img is None :
        print(f"Error unable to read image {img}. \n ")
     else :
-       cv.imshow("Day", img)
+       cv.imshow("Evening", img)
        cv.waitKey(0)
        cv.destroyAllWindows()
 
+
+# Reading Videos in OpenCV
 
