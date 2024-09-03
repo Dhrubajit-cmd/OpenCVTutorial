b1ca00320af3afdc8bef73736dd5ebf6d3641a1e
Dhrubajit Chakravarty <dhrubajitchakravorty@gmail.com>
2024-09-03 23:37:18 +0530

    Commit

---
+++ 2.ResizingAndRescallingFrames.py
@@ -0,0 +1,37 @@
+import cv2 as cv
+
+# Rescaling Image File using OpenCV :
+
+# img = cv.imread("/home/dc/Pictures/Wallpapers/evening.png")
+# cv.imshow("Picture", img)
+
+def rescaleFrame(frame , scale = 0.2) :
+    # This shall work for images,  videos and live video.
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width,height)
+
+    return cv.resize(frame, dimensions, interpolation = cv.INTER_AREA )
+
+video_path = "/home/dc/Videos/video.mp4"
+
+capture = cv.VideoCapture(video_path)  # Int  0(in place of path) : Refers Webcam , 1 : 1st Camera Connected to our system and so on..
+while True :
+    isTrue, frame = capture.read()
+    frame_resized = rescaleFrame(frame)
+
+    cv.imshow("Video_Resized", frame_resized)
+
+    if cv.waitKey(20) & 0xFF==ord('q'):  # 0xFF==ord('q') -> Says that until 'q' key is pressed the video keeps on running.
+        break
+capture.release()
+cv.destroyAllWindows()
+
+# Another method for changing the res of the video :
+def changeRes(width,height):
+    # This works just for the Live Video
+    capture.set(4,width)
+    capture.set(4,height)
+
+
+
