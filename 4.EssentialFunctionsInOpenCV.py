68b993b81c8c597d9bc82c87b877d8b933b6aef3
Dhrubajit Chakravarty <dhrubajitchakravorty@gmail.com>
2024-09-08 23:57:02 +0530

    Commit

--- 4.EssentialFunctionsInOpenCV.py
+++ 4.EssentialFunctionsInOpenCV.py
@@ -1,11 +1,39 @@
 import cv2 as cv
 
 img = cv.imread("/home/dc/Pictures/Wallpapers/evening.png")
-
-cv.imshow('Cat', img)
+dimensions = (1920,1080)
+img_resized = cv.resize(img,dimensions)
+cv.imshow('Pictures', img_resized)
 
 # Converting an image to greyscale :
+grey = cv.cvtColor(img , cv.COLOR_BGR2GRAY)
+dimensions_0 = (1920,1080)
+grey_resized = cv.resize(grey,dimensions_0)
+cv.imshow("Grey", grey_resized)
+# Converting gray to bgr/normal image.
+# image = cv.imread("/home/dc/Pictures/Wallpapers/Grey.png")
+# back_color = cv.cvtColor(image,cv.COLOR_GRAY2BGR)
+# cv.imshow("Normal", back_color)
+
+# Blurring an Image :
+blur = cv.GaussianBlur(img,(3,3), cv.BORDER_DEFAULT)  # Remember, the ksize must always be kept odd.
+dimensions_1 = (1920,1080)
+blur_resized = cv.resize(blur,dimensions_1)
+cv.imshow("Blur", blur_resized)
+
+# Edge Cascade :(Pretty famous in computer vision world)
+canny = cv.Canny(img, threshold1 = 120, threshold2 = 180) # We can also write it as : cv.Canny(img, 120,180)
+dimensions_2 = (1920,1080)
+canny_resized = cv.resize(canny,dimensions_2)
+cv.imshow('Canny Images', canny_resized)
+
+
+# How to Dilate an image using a specific structuring element :
+dilated = cv.dilate(canny, (7,7), iterations = 1)
+dilated_resized = cv.resize(dilated, dimensions )
+cv.imshow("Dilated Images", dilated_resized)
 
 
 
 cv.waitKey(0)
+cv.destroyAllWindows()
